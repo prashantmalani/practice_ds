40e2a6c3f24298bf26806e861dd10e7aa28cc0d9
Prashant Malani <p.malani@gmail.com>
2013-12-27 01:37:24 -0800

    remove random_probs empty directory.
    Random probs directory is vacant. So delete it.
    
    Signed-off-by: Prashant Malani <p.malani@gmail.com>

--- random_probs/min_max_soda.py
+++
@@ -1,5 +0,0 @@
-# In soda water machine:
-# press button A can generate 300-310ml,
-# button B can generate 400-420ml,
-# and button C can generate 500-515ml.
-# Given a number range [min, max], tell if all the volumes of water in the range can be generated.
