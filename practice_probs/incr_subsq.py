b2a9b35811d76c199ec27c52054fe580ad1536e9
Prashant Malani <pmalani@nvidia.com>
2013-12-27 20:13:18 -0800

    incr_subsq: Add problem statement for incr subsequence
    Signed-off-by: Prashant Malani <pmalani@nvidia.com>

---
+++ practice_probs/incr_subsq.py
@@ -0,0 +1,5 @@
+# Given an unsorted array, find the max length of subsequence in which the
+# numbers are in incremental order.
+#
+# For example: If the input array is {7, 2, 3, 1, 5, 8, 9, 6}, a subsequence with the most
+# numbers in incremental order is {2, 3, 5, 8, 9} and the expected output is 5.
